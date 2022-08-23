495d709ac44e862c23385c792ee1f93772605210
jonnathanb2001 <buenopaul111@gmail.com>
2022-08-23 07:58:41 -0500

    Iniciar proyecto PV

--- build/generated-sources/ap-source-output/Modelo/Persona_.java
+++ build/generated-sources/ap-source-output/Modelo/Persona_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-22T19:08:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-23T07:55:32")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
--- build/generated-sources/ap-source-output/Modelo/Producto_.java
+++ build/generated-sources/ap-source-output/Modelo/Producto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-22T19:08:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-23T07:55:32")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
